6a3d187047835d3504b0ea905557f0fd614bd553
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-21 18:01:15 +0300

    feat: implementation of function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	var bracketsMap = new Map(bracketsConfig);
+	var brackets = [];
+
+	for (let i = 0; i < str.length; i++) {
+          str[i] === bracketsMap.get(brackets[brackets.length - 1]) ? brackets.pop() : brackets.push(str[i]);
+	}
+	return brackets.length === 0;
 }
